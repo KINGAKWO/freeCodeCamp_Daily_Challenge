5830773512e4dd998c0363677fac238954f11332
KINGAKWO <kingakwomakembe@gmail.com>
2025-11-16 05:18:49 +0100

    finding GCD

--- GCD/gcd.py
+++ GCD/gcd.py
@@ -1,5 +1,16 @@
-def gcd(x, y):
-    return x if y == 0 else gcd(y, x % y)
+def gcd(a, b):
+    # Handling zeros
+    if a == 0 and b == 0:
+        return 0  
+    if a == 0:
+        return abs(b)
+    if b == 0:
+        return abs(a)
 
+    # Handling  negatives
+    a, b = abs(a), abs(b)
 
-gcd(4, 6)
+    # Euclidean algorithm
+    GCD = a if b == 0 else gcd(b, a % b)
+
+    return GCD
